0163b3708bae395851c002dc1c6b296283b03057
myuzhao <myuzhao@163.com>
2023-05-21 15:37:04 +0800

    change feature to mfcc

--- models/featurizer.py
+++ models/featurizer.py
@@ -8,21 +8,12 @@ class AudioFeaturizer(nn.Module):
         self.sample_rate = sample_rate
         if spec_width != None:
             hop_length = int(sample_rate * audio_time / (spec_width - 1))
-            #hop_length = int(sample_rate * audio_time / (spec_width))
-            self.feat_fun = MelSpectrogram(sample_rate = sample_rate, 
-                                       n_fft = n_fft, 
-                                       hop_length = hop_length,
-                                       f_min = f_min,
-                                       f_max = f_max,
-                                       n_mels = n_mels)
+            melkwargs={"n_fft": n_fft, "hop_length": hop_length, "n_mels": n_mels, "center": False, "f_min": f_min, "f_max": f_max}
         else:
-            self.feat_fun = MelSpectrogram(sample_rate = sample_rate, 
-                                       n_fft = n_fft, 
-                                       f_min = f_min,
-                                       f_max = f_max,
-                                       n_mels = n_mels)
-        
+            melkwargs={"n_fft": n_fft, "hop_length": int(n_fft/2), "n_mels": n_mels, "center": False, "f_min": f_min, "f_max": f_max}
 
+        self.feat_fun = MFCC(sample_rate=self.sample_rate, n_mfcc=n_mels, log_mels=True, melkwargs = melkwargs)
+        
     def forward(self, waveforms):
         feature = self.feat_fun(waveforms)
         feature = feature.transpose(2, 1)
